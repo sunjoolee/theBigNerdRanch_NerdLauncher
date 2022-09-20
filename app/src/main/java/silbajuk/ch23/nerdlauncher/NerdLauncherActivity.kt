7a77754ab5d0b7352f710d706321f30151f84f6f
DESKTOP-4KHJS6O\user <spottypoo124@gmail.com>
2022-09-20 10:33:05 +0900

    CH23 인텐트와 태스크
    NerdLauncher 생성하기

---
+++ app/src/main/java/silbajuk/ch23/nerdlauncher/NerdLauncherActivity.kt
@@ -0,0 +1,19 @@
+package silbajuk.ch23.nerdlauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class NerdLauncherActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nerd_launcher)
+
+        recyclerView = findViewById(R.id.app_recycler_view)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+    }
+}
